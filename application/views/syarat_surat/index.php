ccc485067989b62848e84ba30fbc99ef4b8a0e43
oong26 <mkhalil26122000@gmail.com>
2022-06-27 20:24:38 +0700

    Master alur surat.

--- application/views/syarat_surat/index.php
+++ application/views/syarat_surat/index.php
@@ -3,11 +3,11 @@
         <div class="col-lg-12 ">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item active" aria-current="page"><?=ucwords($this->uri->segment(1))?></li>
-                    <li class="breadcrumb-item " aria-current="page"><a href="<?=site_url("welcome/index")?>">Dashboard</a></li>
+                    <li class="breadcrumb-item active" aria-current="page"><?= ucwords($this->uri->segment(1)) ?></li>
+                    <li class="breadcrumb-item " aria-current="page"><a href="<?= site_url("welcome/index") ?>">Dashboard</a></li>
                 </ol>
             </nav>
-            <?php if ($this->session->flashdata() ) : ?>
+            <?php if ($this->session->flashdata()) : ?>
                 <div class="alert alert-success" role="alert">
                     <?php echo $this->session->flashdata('pesan') ?>
                 </div>
@@ -18,29 +18,29 @@
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-body">
-                <div class="d-flex justify-content-start mb-4">
-                    <a href="<?=site_url('syaratsurat/create')?>" class="btn btn-primary p-3"> <i class="fa fa-plus" aria-hidden="true"></i> Tambah Data</a>
-                </div>
+                    <div class="d-flex justify-content-start mb-4">
+                        <a href="<?= site_url('syaratsurat/create') ?>" class="btn btn-primary p-3"> <i class="fa fa-plus" aria-hidden="true"></i> Tambah Data</a>
+                    </div>
                     <div class="table-responsive">
                         <table class="table table-striped table-bordered" id="example">
                             <thead>
-                            <?php $no = 1; ?>
+                                <?php $no = 1; ?>
                                 <tr>
                                     <th>#</th>
                                     <th>Jenis</th>
-                                    <th>Syarat</th>
+                                    <th>Syarat / Deskripsi</th>
                                     <th>Action</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <?php foreach ($data as $item) : ?>
                                     <tr>
-                                        <td><?=$no++?></td>
+                                        <td><?= $no++ ?></td>
                                         <td><?= ucfirst($item['jenis']) ?></td>
-                                        <td><?=$item['syarat']?></td>
+                                        <td><?= $item['syarat'] ?></td>
                                         <td>
-                                            <a href="<?=base_url('syaratsurat/edit/')?><?=$item['id']?>" class="btn btn-warning"><i class="fa fa-edit"></i></a>
-                                            <a href="<?=base_url('syaratsurat/delete/')?><?=$item['id']?>" onclick="return confirm('Apakah anda yakin ingin menghapus data ?')" class="btn btn-danger"><i class="fa fa-trash"></i></a>
+                                            <a href="<?= base_url('syaratsurat/edit/') ?><?= $item['id'] ?>" class="btn btn-warning"><i class="fa fa-edit"></i></a>
+                                            <a href="<?= base_url('syaratsurat/delete/') ?><?= $item['id'] ?>" onclick="return confirm('Apakah anda yakin ingin menghapus data ?')" class="btn btn-danger"><i class="fa fa-trash"></i></a>
                                         </td>
                                     </tr>
                                 <?php endforeach; ?>
