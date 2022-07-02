7d3e1290b0848ac4d9d7a1ea1bf2e27c80160b68
oong26 <mkhalil26122000@gmail.com>
2022-07-03 00:28:46 +0700

    Action pada penduduk antara view dan hapus jangan dempet

--- application/views/penduduk/index.php
+++ application/views/penduduk/index.php
@@ -3,11 +3,11 @@
         <div class="col-lg-12 ">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item " aria-current="page"><a href="<?=site_url("welcome/index")?>">Dashboard</a></li>
-                    <li class="breadcrumb-item active" aria-current="page"><?=ucwords($this->uri->segment(1))?></li>
+                    <li class="breadcrumb-item " aria-current="page"><a href="<?= site_url("welcome/index") ?>">Dashboard</a></li>
+                    <li class="breadcrumb-item active" aria-current="page"><?= ucwords($this->uri->segment(1)) ?></li>
                 </ol>
             </nav>
-            <?php if ($this->session->flashdata() ) : ?>
+            <?php if ($this->session->flashdata()) : ?>
                 <div class="alert alert-success" role="alert">
                     <?php echo $this->session->flashdata('pesan') ?>
                 </div>
@@ -18,9 +18,9 @@
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-body">
-                <div class="d-flex justify-content-start mb-4">
-                    <a href="<?=site_url('penduduk/create')?>" class="btn btn-primary p-3">Tambah Data</a>
-                </div>
+                    <div class="d-flex justify-content-start mb-4">
+                        <a href="<?= site_url('penduduk/create') ?>" class="btn btn-primary p-3">Tambah Data</a>
+                    </div>
                     <div class="table-responsive">
                         <table class="table">
                             <thead>
@@ -42,26 +42,26 @@
                             </thead>
                             <tbody>
                                 <?php $no = 1; ?>
-                                <?php foreach($data as $item) { ?>
-                                <tr>
-                                    <td><?=$no++?></td>
-                                    <td><?= $item['nik'] ?></td>
-                                    <td><?= $item['nama'] ?></td>
-                                    <!-- <td><?= $item['agama'] ?></td>
+                                <?php foreach ($data as $item) { ?>
+                                    <tr>
+                                        <td><?= $no++ ?></td>
+                                        <td><?= $item['nik'] ?></td>
+                                        <td><?= $item['nama'] ?></td>
+                                        <!-- <td><?= $item['agama'] ?></td>
                                     <td><?= $item['tempat_lahir'] ?></td>
                                     <td><?= $item['tanggal_lahir'] ?></td> -->
-                                    <td><?= $item['jenis_kelamin'] ?></td>
-                                    <!-- <td><?= $item['gol_darah'] ?></td>
+                                        <td><?= $item['jenis_kelamin'] ?></td>
+                                        <!-- <td><?= $item['gol_darah'] ?></td>
                                     <td><?= $item['country_name'] ?></td>
                                     <td><?= $item['pendidikan'] ?></td> -->
-                                    <td><?= $item['pekerjaan'] ?></td>
-                                    <td><?= $item['status_pernikahan'] ?></td>
-                                    <td>
-                                        <a href="<?=base_url('penduduk/detail/')?><?=$item['nik']?>" class="btn btn-info"><i class="fa fa-eye"></i></a>
-                                        <a href="<?=base_url('penduduk/edit/')?><?=$item['nik']?>" class="btn btn-warning"><i class="fa fa-edit"></i></a>
-                                        <a href="<?=base_url('penduduk/delete/')?><?=$item['nik']?>" onclick="return confirm('Apakah anda yakin ingin menghapus data ?')" class="btn btn-danger"><i class="fa fa-trash"></i></a>
-                                    </td>
-                                </tr>
+                                        <td><?= $item['pekerjaan'] ?></td>
+                                        <td><?= $item['status_pernikahan'] ?></td>
+                                        <td>
+                                            <a href="<?= base_url('penduduk/detail/') ?><?= $item['nik'] ?>" class="btn btn-info m-1"><i class="fa fa-eye"></i></a>
+                                            <a href="<?= base_url('penduduk/edit/') ?><?= $item['nik'] ?>" class="btn btn-warning m-1"><i class="fa fa-edit"></i></a>
+                                            <a href="<?= base_url('penduduk/delete/') ?><?= $item['nik'] ?>" onclick="return confirm('Apakah anda yakin ingin menghapus data ?')" class="btn btn-danger m-1"><i class="fa fa-trash"></i></a>
+                                        </td>
+                                    </tr>
                                 <?php } ?>
                             </tbody>
                         </table>
