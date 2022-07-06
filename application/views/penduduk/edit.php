088b7e1f44dc2b190ceeec312285ef9d6aacb1d3
oong26 <mkhalil26122000@gmail.com>
2022-07-07 00:42:30 +0700

    Benerin edit penduduk tempat lahir tapi tampil nama penduduk

--- application/views/penduduk/edit.php
+++ application/views/penduduk/edit.php
@@ -51,7 +51,7 @@
                             <div class="col-lg-6">
                                 <div class="form-group">
                                     <label for="">Tempat Lahir</label>
-                                    <input type="text" name="tempat_lahir" class="form-control <?= form_error('tempat_lahir') != null ? 'is-invalid' : ''; ?>" placeholder="Masukkan tempat lahir lengkap" id="" value="<?= $data['nama'] ?>" required>
+                                    <input type="text" name="tempat_lahir" class="form-control <?= form_error('tempat_lahir') != null ? 'is-invalid' : ''; ?>" placeholder="Masukkan tempat lahir lengkap" id="" value="<?= $data['tempat_lahir'] ?>" required>
                                     <?php echo form_error('tempat_lahir', '<small class="text-danger">', '</small>'); ?>
                                 </div>
                             </div>
@@ -60,7 +60,7 @@
                             <div class="col-lg-6">
                                 <div class="form-group">
                                     <label for="tgl_lahir">Tanggal Lahir</label>
-                                    <input type="date" name="tanggal_lahir" class="form-control <?= form_error('tanggal_lahir') != null ? 'is-invalid' : ''; ?>" placeholder="Masukkan tanggal lahir" id="tgl_lahir" value="<?= date('d F Y', strtotime($data['tanggal_lahir'])) ?>">
+                                    <input type="date" name="tanggal_lahir" class="form-control <?= form_error('tanggal_lahir') != null ? 'is-invalid' : ''; ?>" placeholder="Masukkan tanggal lahir" id="tgl_lahir" value="<?= $data['tanggal_lahir'] ?>" required>
                                     <?php echo form_error('tanggal_lahir', '<small class="text-danger">', '</small>'); ?>
                                 </div>
                             </div>
